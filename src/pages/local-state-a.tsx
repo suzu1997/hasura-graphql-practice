a55c9111d235daa0c3a109b9c534640b2bbd9b4f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-17 14:25:55 +0900

    二つのページを作成

---
+++ src/pages/local-state-a.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { LocalStateA } from '../components/LocalStateA';
+
+const LocalStatePageA: NextPage = () => {
+  return <LocalStateA />;
+};
+
+export default LocalStatePageA;
---
+++ src/pages/local-state-b.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+import { LocalStateB } from '../components/LocalStateB';
+
+const LocalStatePageB: NextPage = () => {
+  return (
+    <LocalStateB />
+  )
+}
+
+export default LocalStatePageB;
